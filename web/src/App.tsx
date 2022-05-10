b91f89641b38801145785b9069c81f8d785021fe
Bruno Fukumori <brunofukumori@gmail.com>
2022-05-10 05:43:54 -0300

    add fetchfeedbacks test

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
-import DarkThemeToggleButton from "./components/DarkThemeToggleButton/DarkThemeToggleButton";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
 import { Widget } from "./components/Widget";
+import { api } from "./lib/api";
 
 export default function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const body = document.querySelector('body');
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  useEffect(()=>{
+    async function fetchFeedbacks() {
+      const response = await api('https://feedget-nwl-return-impulse.vercel.app/feedbacks');
+      return console.log(response)
+    }
+    fetchFeedbacks()
+  },[])
 
   function toggleDarkTheme() {
     setIsDarkTheme(!isDarkTheme);
