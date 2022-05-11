95ff430822c8163d5fe9d86723a739435872d302
Bruno Fukumori <brunofukumori@gmail.com>
2022-05-10 22:43:24 -0300

    add more styles and create dashboard to list feedbacks

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -2,20 +2,24 @@ import {ChatTeardropDots} from 'phosphor-react'
 import { Popover } from '@headlessui/react'
 import { WidgetForm } from './WidgetForm'
 
-export function Widget() {
+interface WidgetProps {
+  onFetchData: () => Promise<void>
+}
+
+export function Widget({onFetchData}: WidgetProps) {
 
   return (
-    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+    <Popover className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end z-10">
       <Popover.Panel>
-        <WidgetForm />
+        <WidgetForm onFetchData={onFetchData}/>
       </Popover.Panel>
-      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group' >
-      <ChatTeardropDots className="w-6 h-6"/>
-      <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
-        <span className='pl-2'></span>
-        Feedback
-        </span>
-    </Popover.Button>
+      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-light-surface_primary dark:focus:ring-offset-dark-surface_primary  focus:ring-brand-500'>
+        <ChatTeardropDots className="w-6 h-6"/>
+          <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear '>
+          <span className='pl-2'></span>
+          Feedback
+          </span>
+      </Popover.Button>
     </Popover>
   )
 }
