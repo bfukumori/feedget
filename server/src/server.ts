7b8c2f9ebc19f12f7e5ac7157a976e8163941bdd
Bruno Fukumori <brunofukumori@gmail.com>
2022-05-11 23:22:39 -0300

    fix paayload too large without body-parser dep

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import express, { json } from 'express';
+import express, { json, urlencoded } from 'express';
 import { routes } from './routes';
 
 const app = express();
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
 app.use(cors());
-app.use(json());
+app.use(json({limit: '50mb'}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, ()=> console.log('Server is running...'))
