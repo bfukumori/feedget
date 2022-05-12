2590209e478a682c4b090d35340031db65293a48
Bruno Fukumori <brunofukumori@gmail.com>
2022-05-11 23:07:19 -0300

    trying to fix payload too big error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { json } from 'express';
 import { routes } from './routes';
 
 const app = express();
-
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 app.use(cors());
 app.use(json());
 app.use(routes);
