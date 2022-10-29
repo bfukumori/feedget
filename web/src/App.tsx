b9ef4ea8840b61c6e5471f5a4fa10aca3c8b2682
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-29 00:22:26 -0300

    remove: heroku api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
-import { Dashboard } from "./components/Dashboard/Dashboard";
-import { Header } from "./components/Header/Header";
-import { Widget } from "./components/Widget";
-import { api } from "./lib/api";
+import { useCallback, useEffect, useState } from 'react';
+import { Dashboard } from './components/Dashboard/Dashboard';
+import { Header } from './components/Header/Header';
+import { Widget } from './components/Widget';
+import { api } from './lib/api';
 
 export interface Feedback {
-  id: string,
+  id: string;
   type: string;
   comment: string;
   screenshot: string;
@@ -16,26 +16,26 @@ export default function App() {
   const body = document.querySelector('body');
   const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
 
-  const fetchFeedbacks = useCallback(async()=>{
-    const response = await api('https://feedget-nwl-return-impulse.herokuapp.com/feedbacks');
-      setFeedbacks(response.data)
-  },[])
+  const fetchFeedbacks = useCallback(async () => {
+    const response = await api('/feedbacks');
+    setFeedbacks(response.data);
+  }, []);
 
-  useEffect(()=>{
-    fetchFeedbacks()
-  },[])
+  useEffect(() => {
+    fetchFeedbacks();
+  }, []);
 
   function toggleDarkTheme() {
     setIsDarkTheme(!isDarkTheme);
-    body?.classList.toggle('dark')
-    body?.classList.toggle('bg-dark-background')
+    body?.classList.toggle('dark');
+    body?.classList.toggle('bg-dark-background');
   }
 
   return (
     <>
-      <Header isDarkTheme={isDarkTheme} toggleDarkTheme={toggleDarkTheme}/>
-      <Widget onFetchData={fetchFeedbacks}/>
-      <Dashboard feedbacks={feedbacks}/>
+      <Header isDarkTheme={isDarkTheme} toggleDarkTheme={toggleDarkTheme} />
+      <Widget onFetchData={fetchFeedbacks} />
+      <Dashboard feedbacks={feedbacks} />
     </>
-  )
+  );
 }
